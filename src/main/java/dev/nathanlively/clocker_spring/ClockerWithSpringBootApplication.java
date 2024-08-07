07c1b4bbf2bcda30ca36ce5db9a05d7668017f73
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2024-08-07 15:54:27 -0700

    first

---
+++ src/main/java/dev/nathanlively/clocker_spring/ClockerWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package dev.nathanlively.clocker_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockerWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClockerWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nathanlively/clocker_spring/ClockerWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nathanlively.clocker_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClockerWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
